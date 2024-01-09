4e4acd3fb79dd2d5e082c93db60c5b51721c2303
idky137 <idky137@gmail.com>
2024-01-09 17:01:21 +0000

    added tests to main. 2b and 2c causing errors

--- src/main.rs
+++ src/main.rs
@@ -268,15 +268,76 @@ fn print_command(co: CommandOutput) {
 // --- main
 // --- used for testing..
 fn main() {
-    println!("Test entry - Starting main:");
-    println!("Test entry - Loading test scenario:");
-    let (_regtest_manager, _cph, _faucet, recipient, _txid) =
-        RT.block_on(async move { scenarios::faucet_funded_recipient_default(100_000).await });
+    println!("Starting Test:");
+    let regtest_network = RegtestNetwork::all_upgrades_active();
 
-    let command_str = "do_user_command";
-    let command_inputs = CommandInput::DoUserCommand(("balance".to_string(), vec![], recipient));
+    // --- Test 1: Scenario:FaucetFundedRecipient
+    println!("Test entry 1: Calling run_command::scenario::faucet_funded_recipient:");
+    let command_str_1 = "scenarios::faucet_funded_recipient";
+    let command_inputs_1 = CommandInput::FaucetFundedRecipient(
+        Some(100_000),
+        Some(100_000),
+        Some(100_000),
+        Pool::Orchard,
+        regtest_network,
+    );
+    let command_output_1 = run_command(command_str_1, command_inputs_1);
+    let regtest_manager: RegtestManager;
+    let _cph: ChildProcessHandler;
+    let _faucet: LightClient;
+    let recipient: LightClient;
+    let _opt1: Option<String>;
+    let _opt2: Option<String>;
+    let _opt3: Option<String>;
+    match command_output_1 {
+        CommandOutput::FaucetFundedRecipient(
+            regtest_manager_v,
+            cph_v,
+            faucet_v,
+            recipient_v,
+            opt1_v,
+            opt2_v,
+            opt3_v,
+        ) => {
+            regtest_manager = regtest_manager_v;
+            _cph = cph_v;
+            _faucet = faucet_v;
+            recipient = recipient_v;
+            _opt1 = opt1_v;
+            _opt2 = opt2_v;
+            _opt3 = opt3_v;
+        }
+        _ => {
+            panic!("Error: Incorrect output");
+        }
+    }
 
-    println!("Test entry - Calling run_command:");
-    let command_output = run_command(command_str, command_inputs);
-    print_command(command_output);
+    //print_command(command_output_1);
+    // --- Test 2a: DoUserCommand:Balance
+    println!("Test entry 2a: Calling run_command::do_user_command::balance:");
+    let command_str_2a = "do_user_command";
+    let command_inputs_2a = CommandInput::DoUserCommand(("balance".to_string(), vec![], recipient));
+    let command_output_2a = run_command(command_str_2a, command_inputs_2a);
+    print_command(command_output_2a);
+
+    // --- Test 2b: DoUserCommand:Balance
+    // println!("Test entry 2b: Calling run_command::do_user_command::balance:");
+    // let command_str_2b = "do_user_command";
+    // let command_inputs_2b = CommandInput::DoUserCommand(("balance".to_string(), vec![], recipient));
+    // let command_output_2b = run_command(command_str_2b, command_inputs_2b);
+    // print_command(command_output_2b);
+
+    // --- Test 3a: GenerateNBlocksReturnNewHeight
+    println!("Test entry 3a: Calling run_command::generate_n_blocks_return_new_height:");
+    let command_str_3a = "generate_n_blocks_return_new_height";
+    let command_inputs_3a = CommandInput::GenerateNBlocksReturnNewHeight(regtest_manager, 0);
+    let command_output_3a = run_command(command_str_3a, command_inputs_3a);
+    print_command(command_output_3a);
+
+    // --- Test 3b: GenerateNBlocksReturnNewHeight
+    // println!("Test entry 3b: Calling run_command::generate_n_blocks_return_new_height:");
+    // let command_str_3b = "generate_n_blocks_return_new_height";
+    // let command_inputs_3b = CommandInput::GenerateNBlocksReturnNewHeight(regtest_manager, 10);
+    // let command_output_3b = run_command(command_str_3b, command_inputs_3b);
+    // print_command(command_output_3b);
 }
